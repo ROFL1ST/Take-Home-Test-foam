e5ec07f920c1067aa5fcfaf80bb4769e5ad3624e
danendra <pr.endra48@gmail.com>
2026-07-30 23:18:18 +0700

    fix: middleware hierrachy at app.js

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(notFound);
-app.use(errorHandler);
+
 
 db.sequelize
     .authenticate()
@@ -35,3 +34,6 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.use(notFound);
+app.use(errorHandler);
