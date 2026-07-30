f858712325d2642aa63c9c9f66db25fec4fc1455
danendra <pr.endra48@gmail.com>
2026-07-30 23:20:27 +0700

    fix: adding valiation in models also

--- backend/models/todo.cjs
+++ backend/models/todo.cjs
@@ -1,25 +1,42 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      // No associations
     }
   }
-  Todo.init({
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+
+  Todo.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Title is required.",
+          },
+        },
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      completed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+      tableName: "Todos",
+      timestamps: true,
+    },
+  );
+
   return Todo;
 };
